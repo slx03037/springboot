0b34aa9fb298aa9e53b851b6102cea3220f23eec
shenlx <597213071@qq.com>
2026-06-23 14:10:23 +0800

    opencv

---
+++ tools/opencv/opencv-advance/src/main/java/com/tools/opencv/advance/web/Application.java
@@ -0,0 +1,17 @@
+package com.tools.opencv.advance.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author shenlx
+ * @description
+ * @date 2026/6/23 11:02
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[]args){
+        SpringApplication.run(Application.class,args);
+    }
+}
+
