646ccfe3bcb7f49ac4fdac08a36583f40573c3d4
shenlx <597213071@qq.com>
2026-06-24 10:56:03 +0800

    file

---
+++ tools/document/file/file-basic/src/main/java/com/tools/file/basic/web/controller/FileController.java
@@ -0,0 +1,40 @@
+package com.tools.file.basic.web.controller;
+
+
+import com.tools.file.basic.web.utils.HttpUtil;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * @program: my-spring-all-com.xinwen.mybatis.node01.service
+ * @description:
+ * @author: shenlx
+ * @create: 2023-07-12 22:09
+ **/
+@RestController
+public class FileController {
+    @GetMapping(value = "/image/review")
+    public ResponseEntity<?> imageReview() throws IOException {
+        File file = new File("D:\\workspace-github-project\\my-spring-all-com.xinwen.mybatis.node01.service\\spring-boot-pdf-excel-word\\spring-boot-file-online\\src\\main\\resources\\1.jpeg");
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            byte[] buf = new byte[fileInputStream.available()];
+            fileInputStream.read(buf);
+            return HttpUtil.getResponseEntity(buf, "inline", "file/1.jpeg");
+        }
+    }
+
+    @GetMapping(value = "/image/down")
+    public ResponseEntity<?> imageDown() throws IOException {
+        File file = new File("D:\\workspace-github-project\\my-spring-all-com.xinwen.mybatis.node01.service\\spring-boot-pdf-excel-word\\spring-boot-file-online\\src\\main\\resources\\1.jpeg");
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            byte[] buf = new byte[fileInputStream.available()];
+            fileInputStream.read(buf);
+            return HttpUtil.getResponseEntity(buf, "attachment", "file/1.jpeg");
+        }
+    }
+}
