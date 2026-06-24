9696e1c7ddd854f741be41e0f0850a6ceab21f32
shenlx <597213071@qq.com>
2026-06-24 11:00:02 +0800

    pdf

--- tools/document/file/file-basic/src/main/java/com/tools/file/basic/web/controller/PdfController.java
+++ tools/document/file/file-basic/src/main/java/com/tools/file/basic/web/controller/PdfController.java
@@ -1,7 +1,7 @@
 package com.tools.file.basic.web.controller;
 
 
-import com.xinwen.file.online.utils.HttpUtil;
+import com.tools.file.basic.web.utils.HttpUtil;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ tools/document/pdf/pdf-box/src/main/java/com/tools/pdf/box/web/Application.java
@@ -0,0 +1,16 @@
+package com.tools.pdf.box.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author shenlx
+ * @description
+ * @date 2026/6/24 10:58
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[]args){
+        SpringApplication.run(Application.class,args);
+    }
+}
